2fd061ddd73dc781662af58d90f62d3641a8338a
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-09 12:30:46 -0700

    Finished function to count words.

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,22 @@
 # put your code here.
+
+def count_words(file):
+	"""Count word occurrences."""
+
+	word_count = {}
+
+	with open(file) as test_file:
+		for line in test_file:
+			line = line.rstrip()
+			words = line.split()
+			for word in words:
+				word_count[word] = word_count.get(word, 0) + 1
+				print(word, ": ", word_count[word])
+		return word_count
+		
+
+count_words("test.txt")
+
+
+
+
